c8f07a8f031ee78fe47a37d68444ebc0164721ad
deaedria <dea.edria@gmail.com>
2021-06-17 19:55:27 +0700

    result

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,18 @@
 const fizzBuzz = function(max) {
-    console.log('Silahkan ubah file fizzbuzz.js');
+    for(let i=1; i<=max; i++){
+        if(i%3 === 0 && i%5 === 0){
+            console.log(i + " FizzBuzz")
+        }
+        else if(i%5 === 0){
+            console.log(i + " Buzz")
+        }
+        else if(i%3 === 0){
+            console.log(i + " Fizz")
+        }
+        else{
+            console.log(i.toString())
+        }
+    }
 }
 
 module.exports = fizzBuzz;
